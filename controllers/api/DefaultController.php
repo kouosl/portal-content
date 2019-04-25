2cb5537da1e9fcb4bb791fc65b05083288509da1
sh4deofsun <by.yalcin.batuhan@gmail.com>
2019-04-25 16:36:33 +0300

    deleting duplicates

--- controllers/api/DefaultController.php
+++ controllers/api/DefaultController.php
@@ -1,18 +1,14 @@
 <?php
 namespace kouosl\content\controllers\api;
-
-
 /**
  * Default controller for the `content` module
  */
-class DefaultController extends \kouosl\base\controllers\api\BaseController
-{
+class DefaultController extends \kouosl\base\controllers\api\BaseController{
     /**
      * Renders the index view for the module
      * @return string
      */
-    public function actionIndex()
-    {
+    public function actionIndex(){
         return $this->render('_index');
     }
 }
